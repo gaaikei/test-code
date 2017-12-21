363db1b7161562c48ff0a06ca15ca6ca0337fa5e
219 <34501333+gaaikei@users.noreply.github.com>
2017-12-21 14:57:06 +0800

    modoify data.py

--- data_providers/data.py
+++ data_providers/data.py
@@ -1,238 +1,179 @@
-import os
 import cv2
+import os
 import random
 import tempfile
 import numpy as np
 from Queue import Queue
 from threading import Thread
 
-from .base_provider import VideosDataset, DataProvider
 
-class Data(VideosDataset):
-    def __init__(self, name, img_type, paths, normalization, sequence_length,
-               crop_size, num_classes, queue_size):
-              """
-              Args:
-                name: str, name of the data (train, test or validation)
-                paths: list, list of string that have the video path and label 
-                  information
-                sequence_length: video clip length
-                crop_size: `tuple`, image resize size (width, height)
-                normalization: `str` or None
-                  None: no any normalization
-                  divide_255: divide all pixels by 255
-                  divide_256: divide all pixels by 256
-                num_classes: `integer`, number of classes that the dataset has
-                queue_size: `integer`, data queue size
-                """
-          self.name             = name
-          self.paths            = paths
-          self.normalization    = normalization
-          self.sequence_length  = sequence_length
-          self.crop_size        = crop_size
-          self.num_classes      = num_classes
-          self.queue            = DataQueue(name, queue_size)
-          self.examples         = None
-          self.img_type         = img_type
-          self._start_data_thread()
+class Data(object):
+	"""docstring for Data"""
+	def __init__(self, name, path, normalization, sequence_length,crop_size,
+		num_classes,queue_size):
+		
+		self.name = name
+		self.path = path
+		self.sequence_length = sequence_length
+		self.crop_size = crop_size
+		self.normalization = normalization
+		self.num_classes = num_classes
+		self.queue = DataQueue(name,queue_size)
+		self.examples = None
+		self._start_data_thread()
 
-    def get_frames_data(self, filename, num_frames_per_clip=16):
-      ''' Given a directory containing extracted frames, return a video clip of
-      (num_frames_per_clip) consecutive frames as a list of np arrays
-      
-      Args
-        num_frames_per_clip: sequence_length of the video clip
-      
-      Returns
-        video: numpy, video clip with shape
-          [sequence_length, height, width, channels]
-      '''
-      video = []
-      s_index = 0
-      for parent, dirnames, files in os.walk(filename):
-        filenames = [ fi for fi in files if fi.endswith((".png", ".jpg", "jpeg")) ]
-        if(len(filenames) < num_frames_per_clip):
-          return None
-        suffix = filenames[0].split('.', 1)[1]
-        filenames_int = [i.split('.', 1)[0] for i in filenames]
-        filenames_int = sorted(filenames_int)
-        s_index = random.randint(0, len(filenames) - num_frames_per_clip)
-        for i in range(s_index, s_index + num_frames_per_clip):
-          image_name = str(filename) + '/' + str(filenames_int[i]) + '.' + suffix
-          # print image_name
-          img = cv2.imread(image_name)
-          img = cv2.resize(img, self.crop_size)
-          if self.normalization:
-            img_data = self.normalize_image(img, self.normalization)
-          video.append(img_data)
-      return video
+	def get_frames(self,filename,sequence_length=16):
+		video = []
+		start = 0
+		for parent, dirnames,files in os.walk(filename):
+			filenames = [file for file in files if file.endswith(".jpeg",".jpg",".png")]
+			if (len(filenames) < sequence_length):
+				return	None
+			suffix = filenames[0].split('.',1)[1]
+			filenames_int = [i.split('.',1)[0] for i in filenames]
+			filenames_int = sorted(filenames_int)
+			start = random.randint(0,len(filenames) - sequence_length)
+			for i in range(start,start+sequence_length):
+				img_name = str(filename) + '/' + str(filenames_int[i]) + '.' + suffix
+				img = cv2.imread(img_name)
+				img = cv2.resize(img,self.crop_size)
+				if self.normalization:
+					img = self.normalize_image(img,self.normalization)
+				video.append(img)
+			return video
 
-    def extract_video_data(self):
-      ''' Single tread to extract video and label information from the dataset
-      '''
-      # Generate one randome index and 
-      while True:
-        index = random.randint(0, len(self.paths)-1)
-        video_path, label = self.paths[index].strip('\n').split()
-        video = self.get_frames_data(video_path, self.sequence_length)
-        if video is not None and len(video) == self.sequence_length:
-          # Put the video into the queue
-          video = np.array(video)
-          label = np.array(int(label))
-          self.queue.put((video, label))
+	def normalize_image(self, img, normalization):
+		if normalization = 'std':
+			img = (img - np.mean(img))/np.std(img)
+		elif normalization = 'divide_256':
+			img = img/256
+		elif normalization = 'divide_255':
+			img = img/255
+		else:
+			raise Exception("please set the norm method")
+		return img
 
-    def _start_data_thread(self):
-      print("Start thread: %s data preparation ..." % self.name)
-      self.worker = Thread(target=self.extract_video_data)
-      self.worker.setDaemon(True)
-      self.worker.start()
+	def get_video_data(self):
+		while True:
+			index = random.randint(0,len(self.paths)-1)
+			video_path,label = self.paths[index].strip('\n').split()
+			video = self.get_frames(video_path, self.sequence_length)
+			if video is not None and len(video)==self.sequence_length:
+				video = np.array(video)
+				label = np.array(int(label))
+				self.queue.put((video,label))
 
-    @property
-    def num_examples(self):
-      if not self.examples:
-        # calculate the number of examples
-        total = 0
-        for line in self.paths:
-          video_path, _ = line.strip('\n').split()
-          for root, dirs, files in os.walk(video_path):
-            total += len(files)
-        self.examples = total / self.sequence_length
-      return self.examples
+	def _start_data_thread(self):
+		print("start thread: %s data preparation ..." % self.name)
+		self.worker = Thread(target=self.get_video_data)
+		self.worker = setDaemon(True)
+		self.worker.start()
 
-    def next_batch(self, batch_size):
-      ''' Get the next batches of the dataset 
-      Args
-        batch_size: video batch size
-      
-      Returns
-        videos: numpy, shape 
-          [batch_size, sequence_length, height, width, channels]
-        labels: numpy
-          [batch_size, num_classes]
-      '''
-      videos, labels = self.queue.get(batch_size)
-      videos = np.array(videos)
-      labels = np.array(labels)
-      labels = self.labels_to_one_hot(labels, self.num_classes)
-      return videos, labels
+	@property
+	def num_examples(self):
+		if not self.examples:
+			total = 0
+			for line in self.paths:
+				video_path, _ = line.strip('\n').split()
+				for root,dirs,files in os.walk(video_path):
+					total += len(files)
+			self.examples = total / self.sequence_length
+		return self.examples
+
+	def next_batch(self, batch_size):
+		videos, labels = self.queue.get(batch_size)
+		videos = np.array(videos)
+		labels = np.array(labels)
+		labels = self.lables_to_one_hot(labels,self.num_classes)
+		return videos,labels
+
+	def labels_to_one_hot(self, labels, num_classes):
+		# labels = labels
+		new_labels = np.zeros((labels.shape[0],num_classes))
+		new_labels[range(labels.shape[0]),labels] = np.ones(labels.shape)
+		return new_labels
+	def labels_for_one_hot(self,labels):
+		return np.argmax(labels,axis=1)
 
 
-class DataQueue():
-    def __init__(self, name, maximum_item, block=True):
-      """
-      Args
-        name: str, data type name (train, validation or test)
-        maximum_item: integer, maximum item that this queue can store
-        block: boolean, block the put or get information if the queue is
-          full or empty
-      """
-      self.name         = name
-      self.block        = block
-      self.maximum_item = maximum_item
-      self.queue        = Queue(maximum_item)
+class DataQueue(object):
+	"""docstring for Data"""
+	def __init__(self, name, max_items,block=True):
+		self.name = name
+		self.block = block
+		self.max_items = max_items
+		self.queue = Queue(max_items)
 
-    @property
-    def queue(self):
-      return self.queue
-
-    @property
-    def name(self):
-      return self.name
-
-    def put(self, data):
-      self.queue.put(data, self.block)
-
-    def get(self, batch_size):
-      '''
-      Args:
-        batch_size: integer, the number of the item you want to get from the queue
-      
-      Returns:
-        videos: list, list of numpy data with shape
-          [sequence_length, height, width, channels]
-        labels: list, list of integer number
-      '''
-      videos = []
-      labels = []
-      for i in range(batch_size):
-        video, label = self.queue.get(self.block)
-        videos.append(video)
-        labels.append(label)
-      return videos, labels
+	@property
+	def queue(self):
+		return self.queue
+	@property
+	def name(self):
+		return self.name
+	def put(self, data):
+		self.queue.put(data,self.block)
+	def get(self,batch_size):
+		videos = []
+		labels = []
+		for i in range(batch_size):
+			video,label = self.queue.get(self.block)
+			videos.append(video)
+			labels.append(label)
+		return videos,labels
+		
 
 
-class DataProvider(DataProvider):
-    def __init__(self, path, img_type, num_classes, validation_set=None, test=False,
-                validation_split=None, normalization=None, crop_size=(64,64),
-                sequence_length=16, train_queue=None, valid_queue=None,
-                test_queue=None, train=False, queue_size=300, **kwargs):
-        """
-        Args:
-          num_classes: the number of the classes
-          validation_set: `bool`.
-          validation_split: `int` or None
-              float: chunk of `train set` will be marked as `validation set`.
-              None: if 'validation set' == True, `validation set` will be
-                  copy of `test set`
-          normalization: `str` or None
-              None: no any normalization
-              divide_255: divide all pixels by 255
-              divide_256: divide all pixels by 256
-          sequence_length: `integer`, video clip length
-          crop_size: `tuple`, the size that you want to reshape the images, (width, height)
-          train: `boolean`, whether we need the training queue or not
-          test: `test`, whether we need the testing queue or not
-          queue_size: `integer`, data queue size , default is 300
-        """
-        self._path            = path
-        self._num_classes     = num_classes
-        self._sequence_length = sequence_length
-        self._crop_size       = crop_size
-        self.img_type         = img_type
+class Dataset(object):
+	def __init__(self, path, num_classes, validation_set=None, test=False,
+		validation_split=None, normalization=None, crop_size=(64,64),
+		train_queue=None, valid_queue=None, test_queue=None,
+		train=False, queue_size=300, **kwargs):
+	"""
+	path: path to data
+	num_classes: number of classes
+	validation_set : 'bool'
+	validation_split : 'int' or None
 
-        if self.img_type=='dynamic':
-          self._path = self.path + 'hmdb51_dynamic/'
-        elif self.img_type=='rgb':
-          self._path = self._path + 'hmdb51_frames/'
-        else:
-          print("Must specify your")
+	"""
+	self._path = path
+	self._num_classes = num_classes
+	self._crop_size = crop_size
+	self._sequence_length = sequence_length
 
-        train_videos_labels   = self.get_videos_labels_lines(
-          os.path.join(self._path, 'train.list'))
-        test_videos_labels    = self.get_videos_labels_lines(
-          os.path.join(self._path, 'test.list'))
+	train_labels = self.get_video_lists(os.path.join(self._path,'train.list'))
+	test_labels = self.get_video_lists(os.path.join(self._path,'test.list'))
 
-        if validation_set and validation_split:
-          random.shuffle(train_videos_labels)
-          valid_videos_labels = train_videos_labels[:validation_split]
-          train_videos_labels = train_videos_labels[validation_split:]
-          self.validation = Data('validation', valid_videos_labels,
-                                normalization, sequence_length,
-                                crop_size, num_classes, queue_size)
-        if train:
-          self.train = Data('train', train_videos_labels,
-                            normalization, sequence_length,
-                            crop_size, num_classes, queue_size)
-        if test:
-          self.test = Data('test', test_videos_labels,
-                          normalization, sequence_length,
-                          crop_size, num_classes, queue_size)
-        if validation_set and not validation_split:
-          self.validation = Data('validation', test_videos_labels,
-                                normalization, sequence_length,
-                                crop_size, num_classes, queue_size)
+	if validation_set and validation_split:
+		random.shuffle(train_labels)
+		valid_labels = train_labels[:validation_split]
+		train_labels = train_labels[validation_split:]
+		self.validation = Data('validation',valid_labels,normalization,
+			sequence_length,crop_size,num_classes,queue_size)
 
-    def get_videos_labels_lines(self, path):
-      # Open the file according to the filename
-      lines = open(path, 'r')
-      lines = list(lines)
-      new_lines = [os.path.join(self._path, line) for line in lines]
-      return new_lines
+	if train:
+		self.train = Data('train',train_labels,normalization,sequence_length,
+			crop_size,num_classes,queue_size)
 
-    @property
-    def data_shape(self):
-      return (self._sequence_length, self._crop_size[1], self._crop_size[0], 3)
+	if test:
+		self.test = Data('test',test_labels,normalization,sequence_length,
+			crop_size,num_classes,queue_size)
 
-    @property
-    def n_classes(self):
-      return self._num_classes
+	if validation_set and not validation_split:
+		self.validation = Data('validation',test_labels,normalization,sequence_length,
+			crop_size,num_classes,queue_size)
+
+
+	def get_video_lists(self, path):
+		lines = open(path, 'r')
+		lines = list(lines)
+		new_lines = [os.path.join(self._path,line) for line in lines]
+		return new_lines
+
+	@property
+	def data_shape(self):
+		return (self._sequence_length,self._crop_size[1],self._crop_size[0],3)
+	@property
+	def n_classes(self):
+		return self._num_classes
+
+
